180800634cd2f5427224cd107209588fd1157bfa
rofinn <rory.finnegan@gmail.com>
2021-09-23 10:04:45 -0700

    Review comment additions.

--- src/lightgbm.jl
+++ src/lightgbm.jl
@@ -2,7 +2,7 @@
 # Since that package requires manually installing the lightgbm binary dependency and
 # several MLJ packages we're keeping this as an optional dependency for now.
 # https://gitlab.invenia.ca/invenia/research/FeatureRelevance.jl/-/issues/6
-using .LightGBM
+using LightGBM
 
 """
     PredictivePowerScore(; k=4)
@@ -17,19 +17,22 @@ Base.@kwdef struct PredictivePowerScore <: Criterion
     k::Int = 4
 end
 
-function evaluate(criterion::PredictivePowerScore, x, y)
+# Dispatch to convert `x` vector to a matrix for LightGBM.
+function evaluate(criterion::PredictivePowerScore, x::Vector{<:Real}, y)
+    return evaluate(criterion, reshape(x, (length(x), 1)), y)
+end
+
+function evaluate(criterion::PredictivePowerScore, x::Matrix{<:Real}, y::Vector{<:Real})
     n = 0
     model_ae = 0.0
     naive_ae = 0.0
 
-    # NOTE: LightGBM only works with Matrix/Vector inputs
-    folds = kfolds(
-        (x isa Vector ? reshape(x, (length(x), 1)) : x, y);
-        k=criterion.k, obsdim=:first
-    )
+    folds = kfolds((x, y); k=criterion.k, obsdim=:first)
 
     for ((train_X, train_y), (test_X, test_y)) in folds
         estimator = LGBMRegression()
+
+        # LightGBM doesn't like array views, so we need to call `collect`
         LightGBM.fit!(estimator, collect(train_X), collect(train_y))
         preds = LightGBM.predict(estimator, collect(test_X))
 

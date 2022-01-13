1a527c2ad968d1f4f899ac4ff03bb1117e55f6ad
Rory Finnegan <rory.finnegan@invenia.ca>
2022-01-13 20:34:03 +0000

    Add positive kwarg to gain/split importance and reduce LightGBM verbosity

--- src/lightgbm.jl
+++ src/lightgbm.jl
@@ -5,16 +5,18 @@
 using LightGBM
 
 """
-    PredictivePowerScore(; k=4)
+    PredictivePowerScore(; k=4, verbosity=-1)
 
 Estimate the [predictive power score](https://towardsdatascience.com/rip-correlation-introducing-the-predictive-power-score-3d90808b9598)
 between two sets of values using a `LightGBM.LGBMRegression` model and `median` naive estimators.
 Note that categorical variables are not currently supported.
 
 - `k` is the number of folds used in the decision tree training.
+- `verbosity` the verbosity level to pass to LightGBM
 """
 Base.@kwdef struct PredictivePowerScore <: Criterion
     k::Int = 4
+    verbosity::Int = -1
 end
 
 # Dispatch to convert `x` vector to a matrix for LightGBM.
@@ -27,14 +29,15 @@ function evaluate(criterion::PredictivePowerScore, x::Matrix{<:Real}, y::Vector{
     model_ae = 0.0
     naive_ae = 0.0
 
+    verbosity = criterion.verbosity
     folds = kfolds((x, y); k=criterion.k, obsdim=:first)
 
     for ((train_X, train_y), (test_X, test_y)) in folds
         estimator = LGBMRegression()
 
         # LightGBM doesn't like array views, so we need to call `collect`
-        LightGBM.fit!(estimator, collect(train_X), collect(train_y))
-        preds = LightGBM.predict(estimator, collect(test_X))
+        LightGBM.fit!(estimator, collect(train_X), collect(train_y); verbosity)
+        preds = LightGBM.predict(estimator, collect(test_X); verbosity)
 
         model_ae += sum(abs.(preds .- test_y))
         naive_ae += sum(abs.(median(train_y) .- test_y))
@@ -48,7 +51,7 @@ function evaluate(criterion::PredictivePowerScore, x::Matrix{<:Real}, y::Vector{
 end
 
 """
-    GradientBoostedImportance(; importance_type, iterations=0)
+    GradientBoostedImportance(; importance_type, iterations=0, positive=false, verbosity=-1)
 
 Fits a `LighGBM.LGBMRegression` estimator to a given set of features / targets, and scores
 the features using [`LGBM_BoosterFeatureImportance`](https://lightgbm.readthedocs.io/en/latest/C-API.html?highlight=gain#c.LGBM_BoosterFeatureImportance).
@@ -58,23 +61,26 @@ the features using [`LGBM_BoosterFeatureImportance`](https://lightgbm.readthedoc
 - `importance_type::Symbol`: Whether to use `:gain` or `:split` importance scoring.
 - `iterations=0`: Maximum number of iterations/boosting to consider. The default
 value of 0 means that a single decision tree is used.
+- `positive`: Only return positive (non-redundant) scores
 """
 Base.@kwdef struct GradientBoostedImportance <: Algorithm
     importance_type::Symbol
-    iterations::UInt = 0
+    iterations::UInt=0
+    positive::Bool=false
+    verbosity::Int=-1
 
-    function GradientBoostedImportance(importance_type::Symbol, iterations=0)
+    function GradientBoostedImportance(importance_type::Symbol, iterations=0, positive=false, verbosity=-1)
         # This seemed simpler than an enum type or multiple wrappers
         if importance_type ∉ (:split, :gain)
             throw(ArgumentError("Supported importance types are :split or :gain"))
         end
 
-        return new(importance_type, UInt(iterations))
+        return new(importance_type, UInt(iterations), positive, verbosity)
     end
 end
 
-GainImportance(; iterations=0) = GradientBoostedImportance(:gain, iterations)
-SplitImportance(; iterations=0) = GradientBoostedImportance(:split, iterations)
+GainImportance(; kwargs...) = GradientBoostedImportance(; importance_type=:gain, kwargs...)
+SplitImportance(; kwargs...) = GradientBoostedImportance(; importance_type=:split, kwargs...)
 
 function selection(alg::GradientBoostedImportance, target, features)
     # NOTE: If we have more than 1 target column LightGBM will error with a
@@ -87,19 +93,28 @@ function selection(
     target::Vector{<:Real},
     features::Matrix{<:Real},
 )
+    verbosity=alg.verbosity
     estimator = LGBMRegression()
-    LightGBM.fit!(estimator, features, target)
+    LightGBM.fit!(estimator, features, target; verbosity)
 
     # Rather than having a separate wrapper for each importance type we just pass
     # a 0/1 for split/gain
     # https://lightgbm.readthedocs.io/en/latest/C-API.html?highlight=gain#c.C_API_FEATURE_IMPORTANCE_GAIN
-    scores = LightGBM.feature_importance_wrapper(
+    selected_scores = LightGBM.feature_importance_wrapper(
         estimator,
         alg.importance_type === :gain,
         alg.iterations,
     )
+    selected_indices = collect(eachindex(selected_scores))
 
-    return collect(eachindex(scores)), scores
+    if alg.positive
+        pos_idx = findall(>(0.0), selected_scores)
+        if length(pos_idx) < length(selected_scores)
+            return selected_indices[pos_idx], selected_scores[pos_idx]
+        end
+    end
+
+    return selected_indices, selected_scores
 end
 
 # Utility function for coercing tables and arrays into the correct LightGBM type
